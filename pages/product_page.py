e461df4b3d6aa36af7a9d2cc806627802a0dd5b0
Dmitry <mmmmm@mmmmm.mmmm>
2020-09-04 22:00:17 +0500

    added test for product page

---
+++ pages/product_page.py
@@ -0,0 +1,19 @@
+from .base_page import BasePage
+from .locators import ProductPageLocators as PPL
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        link = self.browser.find_element(*PPL.ADD_TO_BASKET)
+        link.click()
+        self.solve_quiz_and_get_code()
+
+    def test_correct_item_in_basket(self):
+        name_basket = self.browser.find_elements(
+            *PPL.PRODUCT_NAME_IN_BASKET)[0]
+        name_item = self.browser.find_element(*PPL.PRODUCT_NAME)
+        assert name_basket.text == name_item.text.strip(), "Different product name!"
+        price_basket = self.browser.find_element(
+            *PPL.PRODUCT_PRICE_IN_BASKET)
+        price_item = self.browser.find_element(*PPL.PRODUCT_PRICE)
+        assert price_basket.text == price_item.text, "Different product name!"
--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,7 @@
-import pytest
 from pages.main_page import MainPage
 from pages.login_page import LoginPage
 
 
-#@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = MainPage(browser, link)
@@ -13,7 +11,6 @@ def test_guest_can_go_to_login_page(browser):
     login_page.should_be_login_page()
 
 
-@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
@@ -21,7 +18,6 @@ def test_guest_should_see_login_link(browser):
     page.should_be_login_link()
 
 
-@pytest.mark.skip
 def test_should_be_login_form(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
@@ -29,7 +25,6 @@ def test_should_be_login_form(browser):
     page.should_be_login_form()
 
 
-@pytest.mark.skip
 def test_should_be_register_form(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
@@ -37,7 +32,6 @@ def test_should_be_register_form(browser):
     page.should_be_register_form()
 
 
-@pytest.mark.skip
 def test_should_be_login_url(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
     page = LoginPage(browser, link)
