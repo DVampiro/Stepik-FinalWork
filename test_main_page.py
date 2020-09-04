5dd3aee627b0a3189d4a27d07848ecc4c26b76d6
Dmitry <mmmmm@mmmmm.mmmm>
2020-09-04 14:29:00 +0500

    added tests for login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,17 +1,43 @@
+import pytest
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 
+#@pytest.mark.skip
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    # инициализируем Page Object
     page = MainPage(browser, link)
-    page.open()  # открываем страницу
-    # выполняем метод страницы - переходим на страницу логина
+    page.open()
     page.go_to_login_page()
 
 
+#@pytest.mark.skip
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
+
+#@pytest.mark.skip
+def test_should_be_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+
+
+#@pytest.mark.skip
+def test_should_be_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
+
+
+#@pytest.mark.skip
+def test_should_be_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
